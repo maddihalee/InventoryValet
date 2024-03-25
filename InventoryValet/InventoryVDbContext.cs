9a9be3fca52362adc1de00e55e91fcd2a8a40c7f
Maddi Cook <maddihcook@gmail.com>
2024-03-25 18:44:16 -0500

    setup

---
+++ InventoryValet/InventoryVDbContext.cs
@@ -0,0 +1,31 @@
+﻿namespace InventoryValet.Models;
+using Microsoft.EntityFrameworkCore;
+
+    public class InventoryVDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Item> Items { get; set; }
+
+        public InventoryVDbContext(DbContextOptions<InventoryVDbContext> context) : base(context) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>().HasData(new User[]
+        {
+            new User { Id = 1, Name = "Maddi", Email = "maddi@email.com", FirebaseId = "abc123" }
+        });
+        modelBuilder.Entity<Item>().HasData(new Item[]
+        {
+            new Item { Id = 1, Name = "T-Shirt", Description = "A plain t-shirt", Image = "abc123", Price = 25 }
+        });
+        modelBuilder.Entity<Category>().HasData(new Category[]
+        {
+            new Category { Id = 1, Name = "Men" },
+            new Category { Id = 2, Name = "Women"},
+            new Category { Id = 3, Name = "Kids"}
+        });
+    }
+}
+
